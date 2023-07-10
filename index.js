63c9dee8bb422c40ca4dfc3559bb544d4d60e920
AzuraLulamoon <133188559+AzuraLulamoon@users.noreply.github.com>
2023-07-10 13:24:50 -0500

    cleaning it up with spacing to make things easier to read

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 
-const coffeeMenu = require('./coffee_data');
 //all drinks displayed in an array
+
+const coffeeMenu = require('./coffee_data');
+
 // coffeeMenu.forEach(name => console.log(name));
 
 //drinks that have a price of 5 bucks or lower
+
 let budgetDrinks = coffeeMenu.filter(coffeeMenu => coffeeMenu.price <= 5);
 
 // console.log(budgetDrinks);
 
 //drinks that have a price that is even
+
 let evenDrinks = coffeeMenu.filter(coffeeMenu => coffeeMenu.price % 2 == 0);
 
 // console.log(evenDrinks);
 
-//sum of all drink prices
-// let prices = coffeeMenu.price;
-// const initialValue = 0;
-// const sumWithInitial = prices.reduce(
-//   (accumulator, currentValue) => accumulator + currentValue,
-//   initialValue
-// );
+// sum of all drink prices
 
-// console.log(sumWithInitial);
-// can not figure out how to get this one to work for the life of me
+let prices = [8, 5, 5, 9, 6, 7, 6, 8, 10];
+const initialValue = 0;
+const sumWithInitial = prices.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+);
 
+// console.log(sumWithInitial,"$");
+
+//cant figure out how to pull the data from the coffee_data sheet
+//built it out manually here to demonstrate that the syntax being used
+//DOES work i just cant figure out how to pull the needed data
 
 // seasonal bevage
+
 let seasonalDrinks = coffeeMenu.filter(coffeeMenu => coffeeMenu.seasonal === true);
 
 // console.log(seasonalDrinks);
