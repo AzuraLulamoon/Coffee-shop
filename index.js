54abd45fd151f88e4390afda33f5ab4848fe31cd
AzuraLulamoon <133188559+AzuraLulamoon@users.noreply.github.com>
2023-07-10 10:41:51 -0500

    printed array of all drinks, printed array of drinks equal to or under 5

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 //this is the connection page for later
 const coffeeMenu = require('./coffee_data');
+//displayed as
+console.log(coffeeMenu);
 
-console.log(coffeeMenu)
+let budgetDrinks = coffeeMenu.filter(coffeeMenu => coffeeMenu.price <= 5);
+
+console.log(budgetDrinks);
