bbed6fa00089060e8937e3a0738bf2724d19065d
AzuraLulamoon <133188559+AzuraLulamoon@users.noreply.github.com>
2023-07-10 10:47:47 -0500

    added drinks that are divisable by 2, (even number)

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const coffeeMenu = require('./coffee_data');
 //displayed as
 console.log(coffeeMenu);
 
+//drinks that have a price of 5 bucks or lower
 let budgetDrinks = coffeeMenu.filter(coffeeMenu => coffeeMenu.price <= 5);
 
 console.log(budgetDrinks);
+
+//drinks that have a price that is even
+let evenDrinks = coffeeMenu.filter(coffeeMenu => coffeeMenu.price % 2 == 0);
+
+console.log(evenDrinks);
+
+//
