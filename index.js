e8e6c52c091747788a3c068c77ded2d619da7479
AzuraLulamoon <133188559+AzuraLulamoon@users.noreply.github.com>
2023-07-10 12:05:14 -0500

    all kinds of garabage

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 //this is the connection page for later
 const coffeeMenu = require('./coffee_data');
 //displayed as
-console.log(coffeeMenu);
+// coffeeMenu.forEach(name => console.log(name));
 
 //drinks that have a price of 5 bucks or lower
 let budgetDrinks = coffeeMenu.filter(coffeeMenu => coffeeMenu.price <= 5);
 
-console.log(budgetDrinks);
+// console.log(budgetDrinks);
 
 //drinks that have a price that is even
 let evenDrinks = coffeeMenu.filter(coffeeMenu => coffeeMenu.price % 2 == 0);
 
-console.log(evenDrinks);
+// console.log(evenDrinks);
 
-//
+//sum of all drink prices
+let prices = coffeeMenu.price;
+const initialValue = 0;
+const sumWithInitial = prices.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+);
+
+console.log(sumWithInitial);
+// can not figure out how to get this one to work for the life of me
+
+
+// seasonal bevage
+let seasonalDrinks = coffeeMenu.filter(coffeeMenu => coffeeMenu.seasonal === true);
+
+// console.log(seasonalDrinks);
+
+//seasonal bevage with message
+// use .map?
